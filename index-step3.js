8b3feb3e425c6fab40c9a9a6e8b0f8da88bf7444
tsague <tsagueymele@yahoo.fr>
2016-10-06 16:19:25 +0200

    partie 1 du Step3

--- index-step3.js
+++ index-step3.js
@@ -1,36 +1,67 @@
 $(document).ready(main);
 
 // Fonction principale
-function main(){
-
+function main(){	
 	// Créer et initialiser une variable 'globale' qui 
 	// va stocker le nombre de tentatives restantes.
-
+	var vies = 3;
 	// Créer et initialiser une variable 'globale' qui 
 	// va stocker le nombre "aléatoire" mystère.
-
+	var nbmyst = Math.round(Math.random() * 51); //on veut des nombres entre 0 et 50
+	console.log(nbmyst);
+	
 
 	// == Fonction clickValider == 
 	// Créer la fonction 'clickValider'
-
+	function clickValider(){
 		// Récupérer le contenu de mon input
-
-		// Comparer ce contenu au nombre mystère.
-			/*
-				Si le contenu est égal : 
-				- afficher une alert Gagné
-				- démarrer une nouvelle partie
-			*/
+		var contenu = $("input").val();
+	// Comparer ce contenu au nombre mystère.
+	/*
+		Si le contenu est égal : 
+		- afficher une alert Gagné
+		- démarrer une nouvelle partie
+	*/
+		if(contenu==nbmyst){
+			alert("You Won!");
+			demarrerPartie();
+			//$("input").val(" ");
 
 			// Sinon s'il est supérieur
-			// Afficher une alert Perdu, votre nombre est trop grand
+			// Afficher une alert Perdu, votre nombre est trop grand	
+			} else if(contenu > nbmyst){
+				alert("Perdu, votre nombre est trop Grand!");
+				// Dans les cas où c'est perdu, diminuer le nombre de tentatives
+				vies--;
+			// Actualiser la zone html affichant le nombre de tentatives
+				$(".decompter").text(vies);
+				//partiePerdu();
+				$(".decompter").text(vies);
+				if(vies==0) {demarrerPartie();}
+				$("input").val(" ");
+				//demarrerPartie();
 
+				
 			// Sinon s'il est inférieur
 			// Afficher une alert Perdu, votre nombre est trop petit
-
+			} else {
+					alert("Perdu, votre nombre est trop Petit!" );
 			// Dans les cas où c'est perdu, diminuer le nombre de tentatives
-
+					vies--;
 			// Actualiser la zone html affichant le nombre de tentatives
+					$(".decompter").text(vies);
+					
+					//if(vies==0) { demarrerPartie(); }
+					$("input").val(" ");
+			
+				 	
+			}
+
+			/*if(vie===0){
+				altert("vous avez perdu");
+				$('.decompter').text(vie);
+				location.reload();
+			}*/
 
 			/* 
 				Si le nb de tentatives est égal à 0,
@@ -38,21 +69,67 @@ function main(){
 			 	- afficher que c'est perdu
 			 	- démarrer une nouvelle partie
 			 		+ remettre le nombre de tentatives à sa valeur initiale
-			 		+ générer un nouveau nombre mystère
-			 */
-			
+			 		+ générer un nouveau nombre mystère		 */		
+	}
 	// /Fin == Fonction clickValider == 
-
-
 
 	// Ecrire le code qui va détecter que le bouton 'valider' est cliqué.
 	// Et qui appelle la fonction 'clickValider' défini ci-dessus
+	$('.jouer').on('click', function(){
+		clickValider();
+	});
+
+// revenir aux conditions initiales
+	function demarrerPartie(){
+		vies = 3;
+		nbmyst = Math.round(Math.random() * 51); 
+		console.log(nbmyst);
+		$(".decompter").text(vies);
+	}
+
+	
+	function partiePerdu(){
+		if(vies===0 ){
+			alert("Game Over");
+		}
+	};
 
 
+	
 	/*
 		Concernant la 'refactorisation' : 
 		- créer une fonction 'demarrerPartie'
+
 		- créer une fonction 'partieGagnee'
 		- créer une fonction 'partiePerdue'
 	*/
 }
+
+
+/*
+if ()
+$('div').html('msg')
+vies++;
+if()
+$().html('msg')
+vies++;
+if()
+$().html('msg')
+vies--;
+
+refactorisation
+
+fonction resultat(msg,incr){
+	$('.msg').html(msg);
+	vie = life + incr;
+	}
+
+qui donne
+if()
+resultat('mauvaise reponse', -1)
+if()
+resultat('mauvaise reponse', -1)
+if()
+resultat('bonne reponse', 1)
+
+ */
