fc0b9b7f002e2c2c422efdbc518dec5eff4c9776
tsague <tsagueymele@yahoo.fr>
2016-10-07 15:08:45 +0200

    Step2 Done

--- index-step2.js
+++ index-step2.js
@@ -3,22 +3,12 @@ $(document).ready(pied);
 // Fonction principale
 function pied(){
 
-	// == Fonction clickValider == 
-	// Créer la fonction 'clickValider'
-
 	// Stocker un nombre ENTIER généré "aléatoirement"
 	var stock = Math.round(Math.random() * 51);
 	
 	function clickValider(){
-
 		// Récupérer le contenu de mon input
 		var contenu = $('input').val();
-		console.log(stock);
-		//console.log(stock);
-
-		// Comparer ce contenu à ce nombre
-			// Si le contenu est égal
-			// Afficher une alert Gagné
 		if (contenu ==stock){
 			alert("Gagné !");
 			// Sinon s'il est supérieur
@@ -26,22 +16,13 @@ function pied(){
 			// Afficher une alert Perdu, votre nombre est trop grand
 			alert('Perdu !, votre nombre est trop grand');
 			// Sinon s'il est inférieur
-			} else if (contenu < stock){
+		} else if (contenu < stock){
 			// Afficher une alert Perdu, votre nombre est trop petit
 				alert('Perdu, votre nombre est trop petit');
-			}
-		
-	// /Fin == Fonction clickValider == 
-	}
-			
-
-
-	// Ecrire le code qui va détecter que le bouton 'valider' est cliqué.
-	// Et qui appelle la fonction 'clickValider' défini ci-dessus
+		}
+	}; 
+	
 	$("button").on('click',function(){
 		clickValider();
 	});
-
-
-
-}
+};
