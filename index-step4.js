5a1390435bed30b623635ca43ea1ce84c30cc0fd
tsague <tsagueymele@yahoo.fr>
2016-10-07 14:47:05 +0200

    step 4 done

--- index-step4.js
+++ index-step4.js
@@ -2,62 +2,51 @@ $(document).ready(main);
 
 // Fonction principale
 function main(){
-
-		
-	var choixVies = Number(prompt("choisir le nombre de tentatives"));
-	var afficherVies = $(".count").text(choixVies);
-	var nbmyst = parseInt((Math.random() * (100 - 1)) + 1);
-	
-
-	function clickValider(){
-
-		
-		var contenu = $("input").val(); // Récupérer le contenu de mon input
-	// Comparer ce contenu au nombre mystère.
-		if(contenu==nbmyst){
-			alert("You WON!, Play again?");
-			demarrerPartie();
-			// Sinon s'il est supérieur
-		} else if(contenu > nbmyst){
-			alert("Perdu, votre nombre est trop Grand!");
-			partieActualiser();
-			partiePerdu();		
-			// Sinon s'il est inférieur// Afficher une alert Perdu, votre nombre est trop petit
-		} else {
-			alert("Perdu, votre nombre est trop Petit!" );
-			partieActualiser();
-			partiePerdu();			
-		}	
-
-	};
-
-
-$("button").on('click', function(){
-		clickValider();
-	});
-
-
-// revenir aux conditions initiales
+	//scope
+	var choixVies;
+	var afficherVies;
+	var nbmyst;
+	// revenir aux conditions initiales
 	function demarrerPartie(){
 		choixVies = Number(prompt("choisir le nombre de tentatives"));
-		 afficherVies = $(".count").text(choixVies);
-		nbmyst = parseInt((Math.random() * (100 - 1)) + 1);
+		afficherVies = $(".count").text(choixVies);
+		nbmyst = parseInt((Math.random() * (101 - 1)) + 1);
 	};
 
-	
 	function partiePerdu(){
-		if(choixVies===0){
+		if(choixVies === 0){
 			alert("Game Over");
 			demarrerPartie();
 		}
 	};
 
-	function partieActualiser(){
+	function viesActualiser(){
 		choixVies--;
 		$(".count").text(choixVies);
 	};
 
+	demarrerPartie();
 
+	function clickValider(){
+		var contenu = $("input").val(); // Récupérer le contenu de mon input
+	// Comparer ce contenu au nombre mystère.
+		if(contenu == nbmyst){
+			alert("You WON!, Play again?");
+			demarrerPartie();
+			// Sinon s'il est supérieur
+		} else if(contenu > nbmyst){
+			alert("Perdu, votre nombre est trop Grand!");
+			viesActualiser();
+			partiePerdu();		
+			// Sinon s'il est inférieur// Afficher une alert Perdu, votre nombre est trop petit
+		} else {
+			alert("Perdu, votre nombre est trop Petit!" );
+			viesActualiser();
+			partiePerdu();			
+		}	
+	};
 
-
+	$("button").on('click', function(){
+		clickValider();
+	});
 }
